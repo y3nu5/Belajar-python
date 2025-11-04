7e0892be431fb94641f5c1457bb96115f6c10683
myneos <11231066@student.itk.ac.id>
2025-11-04 11:27:04 +0800

    Day8

---
+++ Day8_function_input.py
@@ -0,0 +1,13 @@
+# def greet(nama):
+#     print(f"Hello, {nama}")
+#     print(f"Welcome to the Python programming course {nama}.")
+
+# greet("Yunus")
+
+def greet(name, location):
+    print(f"Hello, {name}")
+    print(f"what is it like in {location}?")
+
+# greet("Yunus", "Indonesia")
+
+greet(location="Indonesia", name="Yunus")   
